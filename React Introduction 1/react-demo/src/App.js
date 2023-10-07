0f929b38dadc51339c934c52cb39ccd41bb17865
Maneesha-Thakur <manithakur4253@gmail.com>
2023-10-07 07:45:50 +0530

    Intro React

---
+++ React Introduction 1/react-demo/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello this is my first React App</h1>
+      <h1>I am leaning React</h1>
+    </div>
+  );
+}
+
+export default App;
+
